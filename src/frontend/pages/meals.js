0fb17d15eb0185cbbadb8fa71f15d95edf5586de
nuha90 <nuhanajah90@gmail.com>
2020-09-25 20:26:37 +0200

    updated

--- src/frontend/pages/meals.js
+++ src/frontend/pages/meals.js
@@ -23,7 +23,12 @@ window.handleMealsRequest = () => {
   <div class="search-bar">
   <input type="text" id="searchBar" autocomplete="on" placeholder="search for a meal" />
   </div>
-<ul class="allMeals"></ul> `;
+
+
+  <div class="container">
+  <ul class="allMeals"></ul></div?
+    
+ `;
 
   const ul = document.querySelector(".allMeals");
   let mealTitle = document.getElementById("searchBar");
@@ -52,15 +57,21 @@ window.handleMealsRequest = () => {
       const li = document.createElement("li");
       let x = Math.floor(Math.random() * 10 + 1);
       li.innerHTML = `
-     <img src="../assets/random${x}.jpg" class="mealImages" >
-      <div class="w3-container w3-margin-left w3-white">
+     
+      <div class="card">
+      <div class="imgx">
+      <img src="../assets/random${x}.jpg" class="mealImages" >
+      
       <h3>${meal.title}</h3>
       <h3>${meal.price} dkk</h3>
         <button class="w3-button w3-block w3-black w3-margin-bottom w3-margin-left reserve-meal"  data-id='${meal.id}' >
   Book meal</button>
-  <button class="w3-button w3-block w3-blue w3-margin-bottom w3-margin-left reserve-meal"  data-id='${meal.id}' >
+  <button class="w3-button w3-block w3-blue w3-margin-bottom w3-margin-left "  ' >
   <a href="/review/${meal.id}">Rating</a></button>
-  </div>`;
+ </div>
+ </div>
+ </div>
+`;
       ul.appendChild(li);
 
       const selector = `.reserve-meal[data-id='${meal.id}']`;
